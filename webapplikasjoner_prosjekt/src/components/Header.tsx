ce3d93d29e838cc521da1793c4c38b19ba564cdc
Daniel Pihlstrøm Karlsen <danielpk@hiof.no>
2026-09-23 17:19:43 +0200

    La til ul liste i header.

--- webapplikasjoner_prosjekt/src/components/Header.tsx
+++ webapplikasjoner_prosjekt/src/components/Header.tsx
@@ -4,9 +4,11 @@ export function Header() {
       <a href="/create-user"><img src="https://placehold.co/50x50/blue/white" alt="placeholder"/>Create a user account.</a>
       <a href="/"><p>CheckPoint</p></a>
       <nav>
-        <a href="" className="m-5">Option 1</a>
-        <a href="" className="m-5">Option 2</a>
-        <a href="" className="m-5">Option 3</a>
+        <ul>
+          <li><a href="" className="m-5">Option 1</a></li>
+          <li><a href="" className="m-5">Option 2</a></li>
+          <li><a href="" className="m-5">Option 3</a></li>
+        </ul>
       </nav>
     </header>
   )
